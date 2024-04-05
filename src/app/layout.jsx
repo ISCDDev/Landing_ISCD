cff96d774bc56bbf3899671315e4efed02de0e6e
Andres <jaortiz16@espe.edu.ec>
2024-04-05 18:04:50 -0500

    Add of opengraph url

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,6 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "ISCD - Empresa Ecuatoriana de Ciberseguridad",
   description: "ISCD, Somos una comunidad de especialistas tecnológicos dedicados a fortalecer la seguridad cibernética de nuestros clientes desde sus elementos clave.",
+  metadataBase: new URL("https://landingiscd.vercel.app/"),
 };
 
 export default function RootLayout({ children }) {
